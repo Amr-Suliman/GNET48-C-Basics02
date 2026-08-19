823b75e96b560ab418517ce71db15007c123b7d6
Amr Suliman <amrelgohary573@gmail.com>
2026-08-20 02:28:44 +0300

    Add project files.

---
+++ AssignmentTwo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
