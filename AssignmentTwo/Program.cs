32e52199d1aebce0cd296a07ddd78a8002670674
Amr Suliman <amrelgohary573@gmail.com>
2026-08-20 20:22:51 +0300

    Question 0`

--- AssignmentTwo/Program.cs
+++ AssignmentTwo/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            #region Questions 01
+            //string title = "Clean Code";
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine(upperTitle);
+            //Console.WriteLine(title);
+
+            #endregion
         }
     }
 }
