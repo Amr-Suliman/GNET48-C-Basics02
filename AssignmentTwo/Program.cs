0b65c37baade4671b365b9036435b8f7a5052bf7
Amr Suliman <amrelgohary573@gmail.com>
2026-08-20 21:00:13 +0300

    question 04

--- AssignmentTwo/Program.cs
+++ AssignmentTwo/Program.cs
@@ -13,24 +13,26 @@ namespace AssignmentTwo
             //string upperTitle = title.ToUpper();
             //Console.WriteLine(upperTitle);
             //Console.WriteLine(title);
-
             #endregion
 
             #region Question 02
-
             //string title = "Clean Code";
             //string title02 = "Clean Code";
             //Console.WriteLine(object.ReferenceEquals(title, title02));
-
             #endregion
 
             #region Question 03
-
             //StringBuilder sb = new StringBuilder();
             //sb.Append("Book List");
             //sb.Append(" - updated");
             //Console.WriteLine(sb);
+            #endregion
 
+            #region Question 04
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("Book List");
+            //sb.Replace("Book List", "Library");
+            //Console.WriteLine(sb);
             #endregion
 
         }
